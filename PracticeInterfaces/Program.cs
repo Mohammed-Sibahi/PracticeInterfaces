aec3b33ee7a8d1f549348c7b820b21a0752d3baa
Sibahi <sibahi@live.com>
2023-11-21 11:27:35 +0400

    create Report class

--- PracticeInterfaces/Program.cs
+++ PracticeInterfaces/Program.cs
@@ -1,104 +1,39 @@
 ﻿using System;
 
-public interface IMobilePhone
+public interface ILogger
 {
-    public void Sell(double offer);
-    public void Buy(double offer);
-
-    public void PrintReceipt();
+     void Log(string message);
 }
 
-public class Samsung : IMobilePhone
+public class FileLogger : ILogger
 {
-    public void Sell(double offer)
+    public void Log(string message)
     {
-        if (offer >= 100)
-        {
-            Console.WriteLine("Sold!");
-        }
-        else
-        {
-            Console.WriteLine("Offer not accepted.");
-        }
-    }
-    public void Buy(double offer)
-    {
-        if (offer >= 200)
-
-        {
-            Console.WriteLine("Purchased successfully!");
-        }
-        else
-        {
-            Console.WriteLine("Offer not accepted.");
-        }
-    }
-
-    public void PrintReceipt()
-    {
-        Console.WriteLine("Samsung S23 was bought successfully.");
+        Console.WriteLine($"File Logger: {message}");
     }
 }
 
-public class Huawei :  IMobilePhone
+public class ConsoleLogger ILogger
 {
-    public void Sell(double offer)
+    public void Log( string message )
     {
-        if (offer >= 100)
-        {
-            Console.WriteLine("Sold!");
-        }
-        else
-        {
-            Console.WriteLine("Offer not accepted.");
-        }
-    }
-    public void Buy(double offer)
-    {
-        if (offer >= 200)
-        {
-            Console.WriteLine("Purchased!");
-        }
-        else
-        {
-            Console.WriteLine("Offer not accepted.");
-        }
-
-    }
-
-    public void PrintReceipt()
-    {
-        Console.WriteLine("Huawei 60 Pro was bought successfully.");
+        Console.WriteLine ($"Console Logger: {message}");
     }
 }
 
-public class Program
+public class Report 
 {
-    public static void Main(string[] args)
+    private readonly ILogger _logger;
+
+    public Report( ILogger logger )
     {
-        Samsung device1 = new Samsung();
-       Console.Write("Enter offer for selling Samsung S23: ");
-        double sellOffer1 = Convert.ToDouble(Console.ReadLine());
-        device1.Sell(sellOffer1);
-       
-     
-
-        Huawei device2 = new Huawei();
-       Console.WriteLine("Enter offer for selling Huawei 60 Pro: ");
-        double sellOffer2 = Convert.ToDouble(Console.ReadLine());
-        device2.Sell(sellOffer2);
-     
-
-        Console.WriteLine();
-
-       Console.Write("Enter offer for buying Samsung S23: ");
-        double buyOffer1 = Convert.ToDouble(Console.ReadLine());
-        device1.Buy(buyOffer1);
-
-        Console.Write("Enter offer for buying Huawei 60 Pro: ");
-        double buyOffer2 = Convert.ToDouble(Console.ReadLine());
-        device2.Buy(buyOffer2);
-
-       Console.ReadLine();
+        _logger = logger;
     }
+
+    public void GenerateReport( string message )
+    {
+        _logger.Log("Report generated: " + content);
+    }
+
 }
+   
